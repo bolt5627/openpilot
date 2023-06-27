2d87146ffc9a4d714015c0ccb7f38f7aebb58f82
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2023-06-27 15:59:27 +0900

    Update fan_controller.py

--- selfdrive/thermald/fan_controller.py
+++ selfdrive/thermald/fan_controller.py
@@ -110,6 +110,6 @@ class UnoFanController(BaseFanController):
     new_speed = int(interp(max_cpu_temp, [40.0, 80.0], [0, 80]))
 
     if not ignition:
-      new_speed = min(15, new_speed)
+      new_speed = min(80, new_speed)
 
     return new_speed

d2c9a834a9607add6199406e93c31a037959456e
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2023-06-29 15:10:05 +0900

    Update fan_controller.py

--- selfdrive/thermald/fan_controller.py
+++ selfdrive/thermald/fan_controller.py
@@ -21,7 +21,8 @@ class TiciFanController(BaseFanController):
     cloudlog.info("Setting up TICI fan handler")
 
     self.last_ignition = False
-    self.controller = PIDController(k_p=0, k_i=4e-3, k_f=1, rate=(1 / DT_TRML))
+    ###self.controller = PIDController(k_p=0, k_i=4e-3, k_f=1, rate=(1 / DT_TRML))
+    self.controller = PIDController(k_p=0, k_i=4e-3, k_f=1, neg_limit=-80, pos_limit=0, rate=(1 / DT_TRML))
 
   def update(self, cur_temp: float, ignition: bool) -> int:
     self.controller.neg_limit = -(80 if ignition else 30)

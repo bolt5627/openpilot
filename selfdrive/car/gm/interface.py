1337d022ef0b9abb9db25a229c3365aa55c0d5d5
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2023-07-11 03:45:19 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -209,16 +209,26 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kf = 0.000045
       tire_stiffness_factor = 1.0
       
+    ##elif candidate == CAR.BOLT_EUV:
+      ##ret.mass = 1669. + STD_CARGO_KG
+      ##ret.wheelbase = 2.63779
+      ##ret.steerRatio = 17.3
+      ##ret.centerToFront = 2.15  # measured
+      ##tire_stiffness_factor = 1.0
+      ##ret.steerActuatorDelay = 0.10
+      ##CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+
+    ### LEXUS_CTH
     elif candidate == CAR.BOLT_EUV:
-      ret.mass = 1669. + STD_CARGO_KG
-      ret.wheelbase = 2.63779
-      ret.steerRatio = 17.3
-      ret.centerToFront = 2.15  # measured
-      tire_stiffness_factor = 1.0
+      ret.mass = 3108. + STD_CARGO_KG
+      ret.wheelbase = 2.40
+      ret.steerRatio = 17.5
+      ret.centerToFront = 1.81  # LEXUS_CTH
+      tire_stiffness_factor = 0.517
       ret.steerActuatorDelay = 0.10
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
-      
 
+    
     elif candidate == CAR.SILVERADO:
       ret.mass = 2200. + STD_CARGO_KG
       ret.wheelbase = 3.75

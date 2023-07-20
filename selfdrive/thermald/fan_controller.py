7c5580d69d4d37e2086ba3646440e65f58d77ac2
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2023-07-21 03:39:22 +0900

    Update fan_controller.py

--- selfdrive/thermald/fan_controller.py
+++ selfdrive/thermald/fan_controller.py
@@ -108,6 +108,6 @@ class UnoFanController(BaseFanController):
     new_speed = int(interp(max_cpu_temp, [40.0, 60.0, 70.0, 80.0], [0, 20, 50, 80]))
 
     if ignition:
-      new_speed = max(15, new_speed)
+      new_speed = max(32000, new_speed)
 
     return new_speed

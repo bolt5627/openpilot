7c420294f341f82d5e01a945aa5b7377998dcf4d
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2023-07-26 02:35:27 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -84,7 +84,7 @@ class CarInterface(CarInterfaceBase):
 
     ###ret.longitudinalTuning.kpBP = [0., 5., 35.]
     ###ret.longitudinalTuning.kiBP = [0., 12., 27.]
-    ret.longitudinalTuning.kpBP = [0., 5., 35.]
+    ret.longitudinalTuning.kpBP = [0., 5., 20.]
     ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
 
     if candidate in CAMERA_ACC_CAR:

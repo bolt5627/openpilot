cc578beb3a124a154ee78759bde5465a27f0c739
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2023-12-22 14:22:59 +0900

    Update lateral_planner.py

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -17,9 +17,10 @@ from openpilot.common.conversions import Conversions as CV
 
 TRAJECTORY_SIZE = 33
 if EON:
-  CAMERA_OFFSET = -0.06
-else:
-  CAMERA_OFFSET = 0.04
+  CAMERA_OFFSET = -65.06
+  #CAMERA_OFFSET = -0.06
+#else:
+  #CAMERA_OFFSET = 0.04
 
 
 PATH_COST = 1.0

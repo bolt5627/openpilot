104e83abbbcd33902d76156f80f2bef0ce379d8a
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2024-02-12 21:26:22 +0900

    Update carstate.py

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -113,9 +113,9 @@ class CarState(CarStateBase):
       if self.CP.pcmCruise:
         ret.cruiseState.nonAdaptive = cam_cp.vl["ASCMActiveCruiseControlStatus"]["ACCCruiseState"] not in (2, 3)
 ###
-    #if self.CP.enableBsm:
-    #  ret.leftBlindspot = pt_cp.vl["BCMBlindSpotMonitor"]["LeftBSM"] == 1
-    #  ret.rightBlindspot = pt_cp.vl["BCMBlindSpotMonitor"]["RightBSM"] == 1
+    if self.CP.enableBsm:
+      ret.leftBlindspot = pt_cp.vl["BCMBlindSpotMonitor"]["LeftBSM"] == 1
+      ret.rightBlindspot = pt_cp.vl["BCMBlindSpotMonitor"]["RightBSM"] == 1
 ###
     return ret
 
@@ -150,8 +150,8 @@ class CarState(CarStateBase):
       ("ECMAcceleratorPos", 80),
     ]
 ##
-   #if CP.enableBsm:
-   #  messages.append(("BCMBlindSpotMonitor", 10))
+   if CP.enableBsm:
+     messages.append(("BCMBlindSpotMonitor", 10))
 ##
     # Used to read back last counter sent to PT by camera
     if CP.networkLocation == NetworkLocation.fwdCamera:

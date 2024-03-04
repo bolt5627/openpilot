7dc0721fe63b8455ad00bf90ccf05f36d6ca0cf2
Bolt5627 <115066584+bolt5627@users.noreply.github.com>
2024-03-04 20:14:59 +0900

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-while  true; do rm -rf /data/params/d/LiveParameters; sleep 180; done &
+while  true; do rm -rf /data/params/d/LiveParameters; sleep 600; done &
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
